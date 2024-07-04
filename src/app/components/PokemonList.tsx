fdd319706a37f9755807b8de1a3968743dd29e1b
Leejunhyeok03 <ly0609@naver.com>
2024-07-04 19:42:46 +0900

    feat: PokemonList api 연동

--- src/app/components/PokemonList.tsx
+++ src/app/components/PokemonList.tsx
@@ -1,52 +1,41 @@
 "use client";
+
+import axios from "axios";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 function PokemonList() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/pokemons");
+        setPokemons(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <ul className="max-w-[1240px]  m-auto p-4 grid  gap-2 sm:gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
-      <li className="h-52 px-2 border border-white">
-        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
-        <p className="text-white">이상해씨</p>
-        <p className="text-white">도감번호: 1</p>
-      </li>
+    <ul className="max-w-[1240px] m-auto p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:gap-4">
+      {pokemons.map((pokemon) => (
+        <li key={pokemon.id} className="h-48 px-2 border border-white">
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={96}
+            height={96}
+            unoptimized
+            className="mx-auto my-4"
+          />
+          <p className="text-white">{pokemon.korean_name || pokemon.name}</p>
+          <p className="text-white">도감번호: {pokemon.id}</p>
+        </li>
+      ))}
     </ul>
   );
 }
