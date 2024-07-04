03f67091dae046f4a4318c7aee01515065d9c6e9
Leejunhyeok03 <ly0609@naver.com>
2024-07-04 11:29:42 +0900

    feat, style: pokemon api 연동, PokemonList style 완성

---
+++ src/app/components/PokemonList.tsx
@@ -0,0 +1,54 @@
+"use client";
+function PokemonList() {
+  return (
+    <ul className="max-w-[1240px]  m-auto p-4 grid  gap-2 sm:gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+      <li className="h-52 px-2 border border-white">
+        <div className="w-24 h-24 mx-auto my-4 bg-white"></div>
+        <p className="text-white">이상해씨</p>
+        <p className="text-white">도감번호: 1</p>
+      </li>
+    </ul>
+  );
+}
+
+export default PokemonList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import PokemonList from "./components/PokemonList";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="">
+      <h3 className="font-bold text-xl text-[#fff] leading-[4rem] text-center">
+        포켓몬 도감
+      </h3>
+      <PokemonList />
+    </main>
+  );
 }
